e2c4bed6324e9d622198815816d4ba8f1d19ece3
MisaelVillaverde <misaelvilla10@gmail.com>
2020-02-23 00:59:06 -0500

    Add functionality to index.js
    Agregue algunas lineas de codigo a index.js para seguir probando las bases de datos con json

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const fs = require('fs')
 var rawdata = fs.readFileSync('mystore.json')
 var products = JSON.parse(rawdata) // products es un array of objects [i][i]
 
-products.forEach(product => console.log("Product Name: " + product.name))
+function priceDefined(product) {
+    if (typeof product.price === 'undefined') {
+        return "Precio Indefinido"
+    } else {
+        return product.price.$numberInt
+    }
+}
+
+products.forEach(product => console.log(`Product Name: ${product.name}          Product Price: ${priceDefined(product)}`))
